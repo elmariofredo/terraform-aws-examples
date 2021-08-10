20d8079f6ba2360d9123f343f33961c688dbd816
Mario Vejlupek <mario@vejlupek.cz>
2021-08-10 10:29:04 +0200

    Separate buckets into separate file

---
+++ aws-s3-buckets.tf
@@ -0,0 +1,22 @@
+module "aws_s3_bucket_base_some_awesome_tool" {
+  source = "./modules/aws_s3_bucket_base"
+
+  auditlog_bucket_name = module.aws_auditlog.bucket.id
+  bucket               = "some-awesome-tool.org.vejlupek"
+  name                 = "Awesome tool data"
+  read_users = [
+    aws_iam_user.test_tools_write.name,
+  ]
+}
+
+module "aws_s3_bucket_base_fluentbit-logs" {
+  source = "./modules/aws_s3_bucket_base"
+
+  auditlog_bucket_name = module.aws_auditlog.bucket.id
+  bucket               = "fluentbit-logs.logging.org.vejlupek"
+  name                 = "Test bucket for auditing"
+  write_users = [
+    aws_iam_user.test_appslogs.name,
+  ]
+}
+
